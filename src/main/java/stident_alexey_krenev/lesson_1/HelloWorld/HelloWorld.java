35c25832b774c3880e5664b5bbcad328a414c667
Alexey Krenev <alexey.krenev@corp.mail.ru>
2021-06-01 18:21:36 +0300

    Krenev, lesson 1, homework. helloworld, calculators

---
+++ src/main/java/stident_alexey_krenev/lesson_1/HelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+package stident_alexey_krenev.lesson_1.HelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!!");
+    }
+}
