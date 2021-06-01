e08af816f29324061bc7e5ac5d1b89f3c62a527f
Alexey Gorbov <alexey.gorbov@corp.mail.ru>
2021-06-01 22:37:48 +0300

    Alexey Gorbov lesson_1

---
+++ src/main/java/student_alexey_gorbov/lesson_1/Devide.java
@@ -0,0 +1,7 @@
+package student_alexey_gorbov.lesson_1;
+
+public class Devide {
+    public static void main(String[] args){
+        System.out.println(45 / 6);
+    }
+}
---
+++ src/main/java/student_alexey_gorbov/lesson_1/Mult.java
@@ -0,0 +1,7 @@
+package student_alexey_gorbov.lesson_1;
+
+public class Mult {
+    public static void main(String[] args){
+        System.out.println(45 * 43);
+    }
+}
