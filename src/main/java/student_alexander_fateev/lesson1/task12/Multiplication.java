ef7b694db0b35b2907f9a8cd061240077eaebf45
Alexander Fateev <alexander.fateev@corp.mail.ru>
2021-06-02 13:19:36 +0300

    lesson1, task10

---
+++ src/main/java/student_alexander_fateev/lesson1/task12/Multiplication.java
@@ -0,0 +1,8 @@
+package student_alexander_fateev.lesson1.task12;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 48, b = 7;
+        System.out.println(a*b);
+    }
+}
