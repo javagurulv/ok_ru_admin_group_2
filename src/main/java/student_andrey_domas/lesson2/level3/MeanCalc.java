2961adbc60878ac64af5c3cbbfe97327ef8f2bd5
Andrey Domas <andrey.domas@corp.mail.ru>
2021-06-08 20:37:10 +0300

    Andrey Domas lesson 2

---
+++ src/main/java/student_andrey_domas/lesson2/level3/MeanCalc.java
@@ -0,0 +1,21 @@
+package student_andrey_domas.lesson2.level3;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class MeanCalc {
+    public static void main(String[] args) {
+        System.out.print("Enter three space separated numbers: ");
+        Scanner scnr = new Scanner(System.in);
+        final String input = scnr.nextLine();
+
+        String[] strings = input.split(" ");
+
+        float sum = 0;
+        for (String s: strings) {
+            sum += Float.parseFloat(s);
+        }
+
+        System.out.println("Mean: " + sum / strings.length);
+    }
+}
