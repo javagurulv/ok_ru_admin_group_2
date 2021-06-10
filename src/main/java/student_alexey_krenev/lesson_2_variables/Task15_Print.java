ff82da3cf0481b187bc3dfc5a00ad045f6871666
Alexey Krenev <alexey.krenev@corp.mail.ru>
2021-06-10 10:01:38 +0300

    Lesson2, vars. Commit path of homework2

---
+++ src/main/java/student_alexey_krenev/lesson_2_variables/Task15_Print.java
@@ -0,0 +1,7 @@
+package student_alexey_krenev.lesson_2_variables;
+
+public class Task15_Print {
+    public static void main(String[] args) {
+        System.out.print("text2");
+    }
+}
---
+++ src/main/java/student_alexey_krenev/lesson_2_variables/Task_18_SizeMatters.java
@@ -0,0 +1,9 @@
+package student_alexey_krenev.lesson_2_variables;
+
+class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
+
