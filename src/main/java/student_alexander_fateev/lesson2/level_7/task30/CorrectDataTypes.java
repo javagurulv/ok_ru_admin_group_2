85fffa53dae37ceaf97dea316d1310af01ee9804
Alexander Fateev <alexander.fateev@corp.mail.ru>
2021-06-14 17:00:15 +0300

    task30

---
+++ src/main/java/student_alexander_fateev/lesson2/level_7/task30/CorrectDataTypes.java
@@ -0,0 +1,15 @@
+package student_alexander_fateev.lesson2.level_7.task30;
+
+public class CorrectDataTypes {
+    public static void main(String[] args) {
+        int numberOne = 10;
+        double numberTwo = 20.0;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        double sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+}
