ef2d5ac5a68c199c4b105a7d75b19348fd72e8f2
Alexander Fateev <alexander.fateev@corp.mail.ru>
2021-06-14 16:35:53 +0300

    lesson1, task24

---
+++ src/main/java/student_alexander_fateev/lesson2/task24/NoSleepNoFocus.java
@@ -0,0 +1,8 @@
+package student_alexander_fateev.lesson2.task24;
+
+class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
