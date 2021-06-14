a0f6a9100671a0c7e7b336d0a9abc6ff402ce5e2
Alexander Fateev <alexander.fateev@corp.mail.ru>
2021-06-14 17:04:52 +0300

    task32

---
+++ src/main/java/student_alexander_fateev/lesson2/level_7/task32/DemoComment.java
@@ -0,0 +1,12 @@
+package student_alexander_fateev.lesson2.level_7.task32;
+
+// One line comment
+public class DemoComment {
+    public static void main(String[] args) {
+        /*
+        Many
+        Lines
+        Comment
+         */
+    }
+}
