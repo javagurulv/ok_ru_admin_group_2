880ac3230a0b0987544e32dca4d7e7aac49e8f0c
Alexander Fateev <alexander.fateev@corp.mail.ru>
2021-06-14 16:57:07 +0300

    multiple changes

---
+++ src/main/java/student_alexander_fateev/lesson2/level_7/task29/SumTwoNumbers.java
@@ -0,0 +1,14 @@
+package student_alexander_fateev.lesson2.level_7.task29;
+
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 20;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        int sum = numberOne + numberTwo;
+        System.out.println("Sum = " + sum);
+    }
+}
