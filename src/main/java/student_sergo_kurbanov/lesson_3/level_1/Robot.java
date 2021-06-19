d0836aff6020a7eb2f7fa2f628865b3c8168f206
Sergo Kurbanov <s.kurbanov@corp.mail.ru>
2021-06-19 09:36:17 +0300

    s.kurbanov homework lesson 2, level 1-2

---
+++ src/main/java/student_sergo_kurbanov/lesson_3/level_1/Robot.java
@@ -0,0 +1,14 @@
+package student_sergo_kurbanov.lesson_3.level_1;
+
+public class Robot {
+    public Robot() {
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+}
---
+++ src/main/java/student_sergo_kurbanov/lesson_3/level_2/CreateString.java
@@ -0,0 +1,9 @@
+package student_sergo_kurbanov.lesson_3.level_2;
+
+public class CreateString {
+    public static void main(String[] args) {
+        String Name = "Sergo";
+
+        System.out.println(Name);
+    }
+}
