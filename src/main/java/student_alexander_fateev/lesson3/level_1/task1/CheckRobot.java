3ff459462af4b2e128dfd69587e58bf64c591193
Alexander Fateev <alexander.fateev@corp.mail.ru>
2021-06-21 17:36:43 +0300

    task32

---
+++ src/main/java/student_alexander_fateev/lesson3/level_1/task1/CheckRobot.java
@@ -0,0 +1,8 @@
+package student_alexander_fateev.lesson3.level_1.task1;
+
+public class CheckRobot {
+    public static void main(String[] args) {
+        Robot testBot = new Robot();
+        testBot.sayHello();
+    }
+}
---
+++ src/main/java/student_alexander_fateev/lesson3/level_1/task1/Robot.java
@@ -0,0 +1,7 @@
+package student_alexander_fateev.lesson3.level_1.task1;
+
+public class Robot {
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+}
