8436fd7d0575ce4baa06e612ee3f27444349c518
Alexander Fateev <alexander.fateev@corp.mail.ru>
2021-06-22 19:15:52 +0300

    lesson3, level3

--- src/main/java/student_alexander_fateev/lesson3/level_1/task1_6/Robot.java
+++ src/main/java/student_alexander_fateev/lesson3/level_1/task1_6/Robot.java
@@ -1,7 +1,10 @@
 package student_alexander_fateev.lesson3.level_1.task1_6;
 
 public class Robot {
-    public Robot() {
+    String name;
+
+    public Robot(String robotName) {
+        this.name = robotName;
     }
 
     public void sayHello() {
@@ -9,6 +12,6 @@ public class Robot {
     }
 
     public void sayYourName() {
-        System.out.println("My name is ROBO");
+        System.out.println("My name is " + this.name);
     }
 }
--- src/main/java/student_alexander_fateev/lesson3/level_1/task1_6/RobotDemo.java
+++ src/main/java/student_alexander_fateev/lesson3/level_1/task1_6/RobotDemo.java
@@ -2,18 +2,16 @@ package student_alexander_fateev.lesson3.level_1.task1_6;
 
 public class RobotDemo {
     public static void main(String[] args) {
-        Robot testBot = new Robot();
+        Robot testBot = new Robot("Bot");
 
         testBot.sayHello();
         testBot.sayYourName();
 
-        System.out.println("Robot1:");
-        Robot robot1 = new Robot();
+        Robot robot1 = new Robot("Robot1");
         robot1.sayHello();
         robot1.sayYourName();
 
-        System.out.println("Robot2:");
-        Robot robot2 = new Robot();
+        Robot robot2 = new Robot("Robot2");
         robot2.sayHello();
         robot2.sayYourName();
     }
