c054a91d725b11338e7cf1423cac8ac57d102e54
vsavonins <viktors.savonins@proofit.lv>
2021-06-28 15:14:55 +0300

    code review

--- src/main/java/student_alexander_zhukov/lesson_2/level_2/task_3/Book.java
+++ src/main/java/student_alexander_zhukov/lesson_2/level_2/task_3/Book.java
@@ -1,4 +1,7 @@
 package student_alexander_zhukov.lesson_2.level_2.task_3;
 
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
 public class Book {
 }
--- src/main/java/student_alexander_zhukov/lesson_3/level_7/ComputerDemo.java
+++ src/main/java/student_alexander_zhukov/lesson_3/level_7/ComputerDemo.java
@@ -1,5 +1,8 @@
 package student_alexander_zhukov.lesson_3.level_7;
 
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
 class ComputerDemo {
     public static void main(String[] args) {
         Computer dell = new Computer("Dell", "XXX-1");
