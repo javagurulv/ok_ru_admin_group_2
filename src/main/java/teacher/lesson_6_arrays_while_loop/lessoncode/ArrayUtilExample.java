21f7e5867060c86e41ca4b0545be3cbaae3191de
vsavonins <viktors.savonins@proofit.lv>
2021-06-29 12:33:54 +0300

    code review

---
+++ src/main/java/teacher/lesson_6_arrays_while_loop/lessoncode/ArrayUtilExample.java
@@ -0,0 +1,18 @@
+package teacher.lesson_6_arrays_while_loop.lessoncode;
+
+public class ArrayUtilExample {
+
+	public static void main(String[] args) {
+
+		//int[] arr = ArrayUtil.createArray(10);
+
+		ArrayUtil arrayUtil = new ArrayUtil();
+		int[] arr = arrayUtil.createArray(10);
+
+		// Dependency Injection / Inversion of Control
+
+
+	}
+
+
+}
--- src/main/java/teacher/lesson_6_arrays_while_loop/lessoncode/BreakExample.java
+++ src/main/java/teacher/lesson_6_arrays_while_loop/lessoncode/BreakExample.java
@@ -6,6 +6,21 @@ class BreakExample {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
+		int i = 0;
+		while (i < 100) {
+			System.out.println("i = " + i);
+			i++;
+		}
+
+		int number = 0;
+		while (number != 5) {
+			System.out.println("Infinite Loop!");
+			System.out.print("Enter magic number: ");
+			number = sc.nextInt();
+		}
+
+
 		while(true) {
 			System.out.println("Infinite Loop!");
 			System.out.print("Enter magic number: ");
