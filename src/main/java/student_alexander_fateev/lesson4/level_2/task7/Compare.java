748c8da57eb142016e17a0c4c0b054e2d416e605
Alexander Fateev <alexander.fateev@corp.mail.ru>
2021-06-30 18:01:55 +0300

    lesson4, level2

---
+++ src/main/java/student_alexander_fateev/lesson4/level_2/task7/Compare.java
@@ -0,0 +1,14 @@
+package student_alexander_fateev.lesson4.level_2.task7;
+
+class Compare {
+    int firstNumber, secondNumber;
+
+    Compare (int firstNumber, int secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    boolean isEqual () {
+        return firstNumber == secondNumber;
+    }
+}
