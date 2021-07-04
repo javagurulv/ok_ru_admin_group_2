1e84828ee610f9d7c2690b7dad97fd603c730fb6
Sergo Kurbanov <s.kurbanov@corp.mail.ru>
2021-07-04 18:38:37 +0300

    s.kurbanov homework lesson 5, level 1-2

---
+++ src/main/java/student_sergo_kurbanov/lesson_5/level_1/ArrayLength1.java
@@ -0,0 +1,8 @@
+package student_sergo_kurbanov.lesson_5.level_1;
+
+public class ArrayLength1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[1]);
+    }
+}
---
+++ src/main/java/student_sergo_kurbanov/lesson_5/level_2/DoubleRandArray.java
@@ -0,0 +1,29 @@
+package student_sergo_kurbanov.lesson_5.level_2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DoubleRandArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[3];
+
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt();
+        }
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] += 2;
+        }
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
