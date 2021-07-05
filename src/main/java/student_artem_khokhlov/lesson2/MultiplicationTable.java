f7e1c56d08c48a535298dbffff0b1f12f962b053
Artem Hohlov <artem.khokhlov@corp.mail.ru>
2021-07-05 16:03:12 +0300

    added artem khokhlov lesson 2

---
+++ src/main/java/student_artem_khokhlov/lesson2/MultiplicationTable.java
@@ -0,0 +1,25 @@
+package student_artem_khokhlov.lesson2;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int number;
+        int result;
+        Scanner scan = new Scanner(System.in);
+
+        try {
+            number = scan.nextInt();
+        }
+        catch (java.util.InputMismatchException err) {
+            System.out.println("Value is not int. Exit");
+            return;
+        }
+
+        System.out.println("Enter number: ");
+        for(int i = 0; i <=10; i++) {
+            result = number * i;
+            System.out.println(number + " * " + i + " = " + result);
+        }
+    }
+}
