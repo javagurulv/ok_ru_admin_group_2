d94bc67ee674b6bcdd4909c4d2e526c3fbb24773
Sergo Kurbanov <s.kurbanov@corp.mail.ru>
2021-07-08 01:16:23 +0300

    s.kurbanov homework lesson 6, level 1-3

--- src/main/java/student_sergo_kurbanov/lesson_6/level_3/ArrayService.java
+++ src/main/java/student_sergo_kurbanov/lesson_6/level_3/ArrayService.java
@@ -65,6 +65,7 @@ public class ArrayService {
     void sort(int[] arr) {
         int temp = 0;
         boolean sorted = true;
+
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] > arr[i + 1]) {
                 sorted = false;
