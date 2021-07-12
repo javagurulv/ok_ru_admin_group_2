b2d81d3ba81a49a326ef359b651f27bbb0ef6864
Artem Alexandrov <artem.alexandrov@corp.mail.ru>
2021-07-12 22:01:39 +0300

    Artem Aleksandrov. Lesson 5

---
+++ src/main/java/student_artem_aleksandrov/lesson5/level1/level5/ArrayUtil.java
@@ -0,0 +1,37 @@
+package student_artem_aleksandrov.lesson5.level1.level5;
+
+/*
+Создайте класс ArrayUtil.
+Напишите в этом классе метод для создания
+массива целых чисел указанной длины.
+ */
+
+
+import java.util.Random;
+
+class ArrayUtil {
+
+    public int[] createArray(int arrayLength) {
+        int[] array = new int[arrayLength];
+        return array;
+    }
+
+    public void fillArrayWithRandomNumbers(int[] array) {
+        Random random = new Random();
+
+        for (int i=0; i<array.length; i++) {
+            array[i] = random.nextInt();
+        }
+    }
+
+    public int findMaxNumber(int[] array) {
+        int max = array[0];
+
+        for (int i=1; i<array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
---
+++ src/main/java/student_artem_aleksandrov/lesson5/level1/level5/ArrayUtilTest.java
@@ -0,0 +1,39 @@
+package student_artem_aleksandrov.lesson5.level1.level5;
+
+import java.util.Arrays;
+
+public class ArrayUtilTest {
+    public static void main(String[] args) {
+        ArrayUtilTest test = new ArrayUtilTest();
+
+        test.shouldCreateArray();
+        test.shouldFindMaxNumber();
+    }
+
+    public static void shouldCreateArray() {
+        int[] array = new ArrayUtil().createArray(10);
+
+        if (array.length != 10) {
+            System.out.println("Test Failed");
+        } else {
+            System.out.println("Test OK");
+        }
+    }
+
+    public void shouldFindMaxNumber() {
+
+        ArrayUtil Array = new ArrayUtil();
+
+        int[] array = Array.createArray(10);
+        Array.fillArrayWithRandomNumbers(array);
+
+        int max = Arrays.stream(array).max().getAsInt();
+
+        if (Array.findMaxNumber(array) == max) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Failed: %s != %s".formatted(Array.findMaxNumber(array), max));
+        }
+    }
+
+}
