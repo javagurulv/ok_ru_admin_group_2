b68dcf8a08bf4211af845b1eef22a9a9e2c69c33
Artem Hohlov <artem.khokhlov@corp.mail.ru>
2021-07-18 15:37:17 +0300

    added l1.t10-12

---
+++ src/main/java/student_artem_khokhlov/lesson_1/task_12/Multiple.java
@@ -0,0 +1,9 @@
+package student_artem_khokhlov.lesson_1.task_12;
+
+public class Multiple {
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 60;
+        System.out.println("Result: " + (a + b));
+    }
+}
