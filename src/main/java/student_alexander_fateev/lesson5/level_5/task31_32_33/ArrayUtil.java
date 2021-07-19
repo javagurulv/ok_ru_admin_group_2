1863a08b3bf104a5a92917336763eb8f8afdbec1
Alexander Fateev <alexander.fateev@corp.mail.ru>
2021-07-19 17:47:20 +0300

    lesson5, level6

--- src/main/java/student_alexander_fateev/lesson5/level_5/task31_32_33/ArrayUtil.java
+++ src/main/java/student_alexander_fateev/lesson5/level_5/task31_32_33/ArrayUtil.java
@@ -3,8 +3,6 @@ package student_alexander_fateev.lesson5.level_5.task31_32_33;
 class ArrayUtil {
 
     public int[] createArray(int arrayLength) {
-//        int [] numbers = new int[arrayLength];
-
         return new int[arrayLength];
     }
 
