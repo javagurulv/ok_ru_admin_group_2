ef0bbe62e9de17235bf8b530e28543695ba98c73
Alexandr Zhukov <alexandr.zhukov@corp.mail.ru>
2021-08-17 15:41:12 +0300

    lesson_8_objects_equality_reflection, homework

---
+++ src/main/java/student_alexander_zhukov/lesson_8_objects_equality_reflection/level_2/PrivateConstructorDemo.java
@@ -0,0 +1,7 @@
+package student_alexander_zhukov.lesson_8_objects_equality_reflection.level_2;
+
+class PrivateConstructorDemo {
+    public static void main(String[] args) {
+        // PrivateConstructor newConstructor = new PrivateConstructor();
+    }
+}
---
+++ src/main/java/student_alexander_zhukov/lesson_8_objects_equality_reflection/level_2/ProtectedClass.java
@@ -0,0 +1,10 @@
+package student_alexander_zhukov.lesson_8_objects_equality_reflection.level_2;
+
+class ProtectedClass {
+
+    // protected class не имеет смысла
+    private String title;
+    private String author;
+
+
+}
