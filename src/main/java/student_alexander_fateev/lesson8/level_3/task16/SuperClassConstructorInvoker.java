6cea7f8b1ff3efc6afd84dc2d266a605e5c18d1d
Alexander Fateev <alexander.fateev@corp.mail.ru>
2021-09-15 17:45:38 +0300

    lesson8, level3

---
+++ src/main/java/student_alexander_fateev/lesson8/level_3/task16/SuperClassConstructorInvoker.java
@@ -0,0 +1,8 @@
+package student_alexander_fateev.lesson8.level_3.task16;
+
+public class SuperClassConstructorInvoker {
+    public SuperClassConstructorInvoker() {
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
